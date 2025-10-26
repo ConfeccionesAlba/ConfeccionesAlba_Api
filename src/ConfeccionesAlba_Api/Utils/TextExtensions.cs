5ff510e4fcbacccd860e951e9fecee6e5a908012
alex6dj <alexeiagueroalba@gmail.com>
2025-10-26 10:57:15 -0300

    Implement SHA256 string hashing

---
+++ src/ConfeccionesAlba_Api/Utils/TextExtensions.cs
@@ -0,0 +1,28 @@
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ConfeccionesAlba_Api.Utils;
+
+public static class StringExtensions
+{
+    public static string GetSha256Hash(this string str)
+    {
+        return GetHashString(str);
+    }
+
+    private static byte[] GetHash(string inputString)
+    {
+        return SHA256.HashData(Encoding.UTF8.GetBytes(inputString));
+    }
+
+    private static string GetHashString(string inputString)
+    {
+        var sb = new StringBuilder();
+        foreach (var b in GetHash(inputString))
+        {
+            sb.Append(b.ToString("x2"));
+        }
+
+        return sb.ToString();
+    }
+}
