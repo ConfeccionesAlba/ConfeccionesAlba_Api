e2451f0956a46be8d47a15bc64f08236b2dedc1d
alex6dj <alexeiagueroalba@gmail.com>
2025-10-27 16:05:38 -0300

    Create tests for GetItemById.cs

---
+++ test/ConfeccionesAlbaApiTests/Routes/Items/Endpoints/GetItemByIdTest.cs
@@ -0,0 +1,131 @@
+using System.Net;
+using ConfeccionesAlba_Api.Models;
+using ConfeccionesAlba_Api.Routes.Items.Endpoints;
+using ConfeccionesAlbaApiTests.Common;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using AwesomeAssertions;
+using ConfeccionesAlba_Api.Data;
+
+namespace ConfeccionesAlbaApiTests.Routes.Items.Endpoints;
+
+[TestFixture]
+[TestOf(typeof(GetItemById))]
+public class GetItemByIdTest
+{
+    private DbContextFactoryFixture _fixture;
+    private ApplicationDbContext _context;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _fixture = new DbContextFactoryFixture();
+        _context = _fixture.GetDbContext();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _context.Dispose();
+    }
+
+    [Test]
+    public async Task Handle_InvalidItemId_ReturnsBadRequest()
+    {
+        // Arrange
+        int invalidId = 0; // Invalid ID (less than 1)
+
+        // Act
+        var result = await GetItemById.Handle(_context, invalidId);
+
+        // Assert
+        result.Should().NotBeNull();
+
+        var badRequestResult = result.Result as BadRequest<ApiResponse>;
+        badRequestResult.Should().NotBeNull();
+        badRequestResult.Value.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        badRequestResult.Value.IsSuccess.Should().BeFalse();
+        badRequestResult.Value.ErrorMessages.Should().Contain("Invalid item Id");
+    }
+
+    [Test]
+    public async Task Handle_NonExistentItemId_ReturnsNotFound()
+    {
+        // Arrange
+        int nonExistentId = 999; // ID that doesn't exist in the database
+
+        // Act
+        var result = await GetItemById.Handle(_context, nonExistentId);
+
+        // Assert
+        result.Should().NotBeNull();
+
+        var notFoundResult = result.Result as NotFound<ApiResponse>;
+        notFoundResult.Should().NotBeNull();
+        notFoundResult.Value.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        notFoundResult.Value.IsSuccess.Should().BeFalse();
+        notFoundResult.Value.ErrorMessages.Should().Contain("Item not found");
+    }
+
+    [Test]
+    public async Task Handle_ValidItemId_ReturnsOkWithItem()
+    {
+        // Arrange
+        // Add a test item to the database
+        var testItem = new Item
+        {
+            Name = "Test Item",
+            Description = "Test Description",
+            CategoryId = 1,
+            PriceReference = 10.99m,
+            ImageUrl = "test.jpg",
+            IsVisible = true,
+            CreatedOn = DateTime.UtcNow,
+            UpdatedOn = DateTime.UtcNow
+        };
+
+        await _context.Items.AddAsync(testItem);
+        await _context.SaveChangesAsync();
+
+        // Act
+        var result = await GetItemById.Handle(_context, testItem.Id);
+
+        // Assert
+        result.Should().NotBeNull();
+
+        var okResult = result.Result as Ok<ApiResponse>;
+        okResult.Should().NotBeNull();
+        okResult.Value.StatusCode.Should().Be(HttpStatusCode.OK);
+        okResult.Value.IsSuccess.Should().BeTrue();
+        okResult.Value.Result.Should().BeOfType<Item>();
+
+        var returnedItem = okResult.Value.Result as Item;
+        returnedItem.Should().NotBeNull();
+        returnedItem.Id.Should().Be(testItem.Id);
+        returnedItem.Name.Should().Be(testItem.Name);
+        returnedItem.Description.Should().Be(testItem.Description);
+        returnedItem.PriceReference.Should().Be(testItem.PriceReference);
+        returnedItem.ImageUrl.Should().Be(testItem.ImageUrl);
+        returnedItem.IsVisible.Should().Be(testItem.IsVisible);
+    }
+
+    [Test]
+    public async Task Handle_DatabaseError_ReturnsInternalServerError()
+    {
+        // Arrange
+        // Simulate a database error by disposing the context
+        await _context.DisposeAsync();
+
+        // Act
+        var result = await GetItemById.Handle(new ApplicationDbContext(new DbContextOptions<ApplicationDbContext>()), 1);
+
+        // Assert
+        result.Should().NotBeNull();
+
+        var internalErrorResult = result.Result as InternalServerError<ApiResponse>;
+        internalErrorResult.Should().NotBeNull();
+        internalErrorResult.Value.StatusCode.Should().Be(HttpStatusCode.InternalServerError);
+        internalErrorResult.Value.IsSuccess.Should().BeFalse();
+        internalErrorResult.Value.ErrorMessages.Should().HaveCount(1);
+    }
+}
