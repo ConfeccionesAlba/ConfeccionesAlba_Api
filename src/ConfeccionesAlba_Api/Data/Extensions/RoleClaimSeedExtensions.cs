f1e5cabe982b97edd47f1829142a9e08c880a270
alex6dj <alexeiagueroalba@gmail.com>
2025-11-05 20:00:03 -0300

    Add missing role permissions for image creation and deletion

--- src/ConfeccionesAlba_Api/Data/Extensions/RoleClaimSeedExtensions.cs
+++ src/ConfeccionesAlba_Api/Data/Extensions/RoleClaimSeedExtensions.cs
@@ -26,6 +26,9 @@ public static class RoleClaimSeedExtensions
             new IdentityRoleClaim<string> { Id = 1008, RoleId = adminRoleId, ClaimType = CustomClaimTypes.Permission, ClaimValue = Permissions.ItemsCreate.ToName() },
             new IdentityRoleClaim<string> { Id = 1009, RoleId = adminRoleId, ClaimType = CustomClaimTypes.Permission, ClaimValue = Permissions.ItemsUpdate.ToName() },
             new IdentityRoleClaim<string> { Id = 1010, RoleId = adminRoleId, ClaimType = CustomClaimTypes.Permission, ClaimValue = Permissions.ItemsDelete.ToName() },
+            
+            new IdentityRoleClaim<string> { Id = 1011, RoleId = adminRoleId, ClaimType = CustomClaimTypes.Permission, ClaimValue = Permissions.ImagesCreate.ToName() },
+            new IdentityRoleClaim<string> { Id = 1012, RoleId = adminRoleId, ClaimType = CustomClaimTypes.Permission, ClaimValue = Permissions.ImagesDelete.ToName() },
 
             // Publisher Role
             new IdentityRoleClaim<string> { Id = 2001, RoleId = publisherRoleId, ClaimType = CustomClaimTypes.Permission, ClaimValue = Permissions.CategoriesCreate.ToName() },
@@ -34,7 +37,10 @@ public static class RoleClaimSeedExtensions
 
             new IdentityRoleClaim<string> { Id = 2004, RoleId = publisherRoleId, ClaimType = CustomClaimTypes.Permission, ClaimValue = Permissions.ItemsCreate.ToName() },
             new IdentityRoleClaim<string> { Id = 2005, RoleId = publisherRoleId, ClaimType = CustomClaimTypes.Permission, ClaimValue = Permissions.ItemsUpdate.ToName() },
-            new IdentityRoleClaim<string> { Id = 2006, RoleId = publisherRoleId, ClaimType = CustomClaimTypes.Permission, ClaimValue = Permissions.ItemsDelete.ToName() }
+            new IdentityRoleClaim<string> { Id = 2006, RoleId = publisherRoleId, ClaimType = CustomClaimTypes.Permission, ClaimValue = Permissions.ItemsDelete.ToName() },
+            
+            new IdentityRoleClaim<string> { Id = 2007, RoleId = publisherRoleId, ClaimType = CustomClaimTypes.Permission, ClaimValue = Permissions.ImagesCreate.ToName() },
+            new IdentityRoleClaim<string> { Id = 2008, RoleId = publisherRoleId, ClaimType = CustomClaimTypes.Permission, ClaimValue = Permissions.ImagesDelete.ToName() }
         );
     }
 }
