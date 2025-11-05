fe1ea096e14ae5b36ae36411e80100035bac67fe
alex6dj <alexeiagueroalba@gmail.com>
2025-11-05 20:07:42 -0300

    Add AddNewImageModelAndPermissions migration for changes in Image and role permissions

---
+++ src/ConfeccionesAlba_Api/Migrations/20251105230359_AddNewImageModelAndPermissions.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ConfeccionesAlba_Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewImageModelAndPermissions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Items");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ImageId",
+                table: "Items",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Url = table.Column<string>(type: "character varying(250)", maxLength: 250, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoleClaims",
+                columns: new[] { "Id", "ClaimType", "ClaimValue", "RoleId" },
+                values: new object[,]
+                {
+                    { 1011, "permission", "images:create", "f4dbaa01-dcae-40a5-844e-d8a719393af4" },
+                    { 1012, "permission", "images:delete", "f4dbaa01-dcae-40a5-844e-d8a719393af4" },
+                    { 2007, "permission", "images:create", "1dacfc02-50b8-43da-a050-b0fa556224b7" },
+                    { 2008, "permission", "images:delete", "1dacfc02-50b8-43da-a050-b0fa556224b7" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_ImageId",
+                table: "Items",
+                column: "ImageId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Images_ImageId",
+                table: "Items",
+                column: "ImageId",
+                principalTable: "Images",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Images_ImageId",
+                table: "Items");
+
+            migrationBuilder.DropTable(
+                name: "Images");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Items_ImageId",
+                table: "Items");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoleClaims",
+                keyColumn: "Id",
+                keyValue: 1011);
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoleClaims",
+                keyColumn: "Id",
+                keyValue: 1012);
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoleClaims",
+                keyColumn: "Id",
+                keyValue: 2007);
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoleClaims",
+                keyColumn: "Id",
+                keyValue: 2008);
+
+            migrationBuilder.DropColumn(
+                name: "ImageId",
+                table: "Items");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Items",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
