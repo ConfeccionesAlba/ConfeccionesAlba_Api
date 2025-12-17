af0f5b0d96b495d7042e42b0d9c7f5a297f7eea2
alex6dj <alexeiagueroalba@gmail.com>
2025-12-17 15:37:44 -0300

    refactor(api): remove unused weather forecast endpoint and record

--- src/ConfeccionesAlba_Api/Program.cs
+++ src/ConfeccionesAlba_Api/Program.cs
@@ -148,25 +148,6 @@ app.UseCors("SpecificOrigins");
 app.UseAuthentication();
 app.UseAuthorization();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
-
 app.MapCategoriesEndpoints();
 app.MapItemsEndpoints();
 app.MapAuthEndpoints();
@@ -178,9 +159,4 @@ return;
 string RedactSensitiveInfo(string connString)
 {
     return string.IsNullOrEmpty(connString) ? connString : connString.GetSha256Hash();
-}
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
